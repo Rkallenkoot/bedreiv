f02f87bec6a0d0421baa2a8c1729b8ed1b1dd8fc
Roelof Kallenkoot <roelof000@gmail.com>
2015-06-19 14:00:21 +0200

    Werkende login shit

---
+++ src/auth/Acl.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace auth;
+
+use Zend\Permissions\Acl\Acl as ZendAcl;
+
+/**
+ * ACL for Slim Auth Implementation Example.
+ */
+class Acl extends ZendAcl
+{
+	protected $defaultPrivilege = array('GET');
+
+	public function __construct()
+	{
+		// APPLICATION ROLES
+		$this->addRole('guest');
+
+		// member role "extends" guest, meaning the member role will get all of
+		// the guest role permissions by default
+		$this->addRole('member', 'guest');
+		$this->addRole('admin');
+
+		// APPLICATION RESOURCES
+		// Application resources == Slim route patterns
+		$this->addResource('/');
+		$this->addResource('/login');
+		$this->addResource('/logout');
+		$this->addResource('/bami');
+		$this->addResource('/admin');
+
+		// APPLICATION PERMISSIONS
+		// Now we allow or deny a role's access to resources.
+		// The third argument is 'privilege'. In Slim Auth privilege == HTTP method
+		$this->allow('guest', '/', $this->defaultPrivilege);
+		$this->allow('guest', '/login', array('GET', 'POST'));
+		$this->allow('guest', '/logout', $this->defaultPrivilege);
+
+		$this->allow('member', '/bami', $this->defaultPrivilege);
+
+		// This allows admin access to everything
+		$this->allow('admin');
+	}
+}
---
+++ src/models/User.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace models;
+
+use \models\BaseModel;
+use PDO;
+
+class User extends BaseModel {
+
+	public function findByUsername($username){
+		$stmt = $this->dbh->prepare("SELECT * FROM user WHERE username = :username");
+		$stmt->execute(array(
+			'username' => $username));
+
+		return $stmt->fetch();
+	}
+
+	public function insertUser($username, $passwordhash, $role){
+		$stmt = $this->dbh->prepare("INSERT INTO user (username, password, role)
+				VALUES (:username, :password, :role)");
+
+		// Returns true when inserted succesfully
+		return $stmt->execute(array(
+			'username' => $username,
+			'password' => $passwordhash,
+			'role' => $role
+			));
+
+	}
+
+}
