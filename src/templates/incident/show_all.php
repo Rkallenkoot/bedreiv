3b2274fe54def2af6b48a01bc050f3448e468c40
Roelof Kallenkoot <roelof000@gmail.com>
2015-06-23 20:53:54 +0200

    random button in incidenten view

--- src/templates/incident/show_all.php
+++ src/templates/incident/show_all.php
@@ -1,4 +1,3 @@
-<!-- Shows a list of incident items -->
 <?php
 include '../templates/partials/header.php';
 include '../templates/partials/menu.php';
@@ -6,7 +5,7 @@ include '../templates/partials/menu.php';
 ?>
 
 <div class="container-fluid">
-	<div class="row"><button class="btn btn-danger btn-success btn-default btn-info btn-warning"></button>
+	<div class="row">
 		<?php include '../templates/partials/sidenav.php'; ?>
 
 		<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
