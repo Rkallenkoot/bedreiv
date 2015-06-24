7ec25a24c06c631c24057409462601eaab09bee9
Thijsku <thijs.kuilman@gmail.com>
2015-06-24 12:04:51 +0200

    New incident button link gefixt

--- src/templates/incident/show_all.php
+++ src/templates/incident/show_all.php
@@ -9,7 +9,7 @@ include '../templates/partials/menu.php';
 		<?php include '../templates/partials/sidenav.php'; ?>
 
 		<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-			<h2 class="sub-header">Incidenten <a class="pull-right btn btn-success" href="/incident_new">Nieuw Incident Registreren</a></h2>
+			<h2 class="sub-header">Incidenten <a class="pull-right btn btn-success" href="/questionnaire">Nieuw Incident Registreren</a></h2>
 
 			<div class="table-responsive">
 				<table class="table table-striped">
