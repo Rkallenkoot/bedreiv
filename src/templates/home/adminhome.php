69ea3606c22eda938fdf5213e9157bec08bc807a
Roelof Kallenkoot <roelof000@gmail.com>
2015-06-24 22:26:07 +0200

    Verschillende views per user role

---
+++ src/templates/home/adminhome.php
@@ -0,0 +1,107 @@
+<?php
+include '../templates/partials/header.php';
+include '../templates/partials/menu.php';
+
+?>
+
+<div class="container-fluid">
+	<div class="row">
+		<?php include '../templates/partials/sidenav.php'; ?>
+		<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+			<h2 class="sub-header">Incidenten rapportage afgelopen maand</h2>
+			<div class="col-md-6">
+				<h2 class="sub-header">Incidenten per Dag per Categorie</h2>
+				<canvas id="chartDate"></canvas>
+			</div>
+			<div class="col-md-5 col-md-offset-1">
+				<h2 class="sub-header">Incidenten op Categorie</h2>
+				<div class="col-md-10">
+					<canvas id="chartCategory" height="150px"></canvas>
+				</div>
+				<div id="categoryLegend" class="col-md-2">
+				</div>
+			</div>
+			<hr>
+			<?php // Hier kunnen we verder met meer stuff?>
+		</div>
+	</div>
+
+	<style>
+		.pie-legend {
+			list-style: none;
+			position: absolute;
+			right: 8px;
+			top: 0;
+		}
+		.pie-legend li {
+			display: block;
+			padding-left: 30px;
+			position: relative;
+			margin-bottom: 4px;
+			border-radius: 5px;
+			padding: 2px 8px 2px 28px;
+			font-size: 14px;
+			cursor: default;
+			-webkit-transition: background-color 200ms ease-in-out;
+			-moz-transition: background-color 200ms ease-in-out;
+			-o-transition: background-color 200ms ease-in-out;
+			transition: background-color 200ms ease-in-out;
+		}
+		.pie-legend li:hover {
+			background-color: #fafafa;
+		}
+		.pie-legend li span {
+			display: block;
+			position: absolute;
+			left: 0;
+			top: 0;
+			width: 20px;
+			height: 100%;
+			border-radius: 3px;
+		}
+	</style>
+	<script src="/js/chart.min.js"></script>
+	<script>
+	// Self invoking function - Stuff to generate charts
+	(function(){
+		var helpers = Chart.helpers;
+		Chart.defaults.global.responsive = true;
+		var data = <?=$chartCategory?>;
+		var canvas = document.getElementById('chartCategory');
+		var ctx = document.getElementById("chartCategory").getContext("2d");
+		var chartCategory = new Chart(ctx).Pie(data, { tooltipTemplate : "<%if (label){%><%=label%>: <%}%><%= value %> incidenten", animation: false });
+
+		var legendHolder = document.createElement('div');
+		legendHolder.innerHTML = chartCategory.generateLegend();
+		legendHolder.className = "list-group";
+		// Include a html legend template after the module doughnut itself
+		helpers.each(legendHolder.firstChild.childNodes, function(legendNode, index){
+			helpers.addEvent(legendNode, 'mouseover', function(){
+				var activeSegment = chartCategory.segments[index];
+				activeSegment.save();
+				activeSegment.fillColor = activeSegment.highlightColor;
+				chartCategory.showTooltip([activeSegment]);
+				activeSegment.restore();
+			});
+		});
+		helpers.addEvent(legendHolder.firstChild, 'mouseout', function(){
+			chartCategory.draw();
+		});
+		document.getElementById('categoryLegend').appendChild(legendHolder.firstChild);
+	})();
+
+	(function(){
+		// Line chart
+		var helpers = Chart.helpers;
+		var data = <?=$chartDate?>;
+
+		var canvas = document.getElementById('chartDate');
+		var ctx = document.getElementById("chartDate").getContext("2d");
+		var dateChart = new Chart(ctx).Line(data, { multiTooltipTemplate: "<%= datasetLabel %> - <%= value %> incidenten", animation: false });
+	})();
+
+</script>
+
+<?php
+include '../templates/partials/footer.php';
+?>
--- src/templates/partials/sidenav.php
+++ src/templates/partials/sidenav.php
@@ -1,9 +1,11 @@
 <div class="col-sm-3 col-md-2 sidebar">
 	<ul class="nav nav-sidebar">
-		<li><a href="/">Dashboard</a></li>
+		<li class="<?php echo ($uri == '/')?'active':'';?>"><a href="/">Dashboard</a></li>
 		<li class="<?php echo (strstr($uri,'incidents'))?'active':'';?>"><a href="/incidents/all">Incidents <span class="badge pull-right"><?=$open?></span></a></li>
+		<?php if($role == 'admin'): ?>
 		<li class="<?php echo (strstr($uri,'users'))?'active':'';?>"><a href="/users/all">Users</a></li>
 		<li class="<?php echo (strstr($uri,'hardware'))?'active':'';?>"><a href="/configs/hardware/all">Hardware</a></li>
 		<li class="<?php echo (strstr($uri,'software'))?'active':'';?>"><a href="/configs/software/all">Software</a></li>
+	<?php endif;?>
 	</ul>
 </div>
